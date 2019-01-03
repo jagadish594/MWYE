29b8e83d2f1f72ffe0c120fb9331eeb8a01f9f7f
jagadish594 <jagadish594@gmail.com>
2019-01-03 09:50:18 -0800

    Jan 3rd update

--- mwye_v3/src/components/AccordionBox.js
+++ mwye_v3/src/components/AccordionBox.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import '../styles/Accordion.css';
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 
 const AccordionBox = () => {
-    
     console.log("Hello");
     return(
         <div className="Container">        
--- mwye_v3/src/components/CompareFoodByNutrients.js
+++ mwye_v3/src/components/CompareFoodByNutrients.js
@@ -60,6 +60,7 @@ class CompareFoodByNutrients extends Component {
         }
       ],
       selectedNutrients: new Set(),
+      basicNutrientInfo: "",
       isCheckBox509Checked: false,
       isCheckBox518Checked: false,
       isCheckBox516Checked: false,
@@ -67,17 +68,20 @@ class CompareFoodByNutrients extends Component {
       isCheckBox514Checked: false,
       isCheckBox513Checked: false
     }
+
     this.toggleChange509 = this.toggleChange509.bind(this);
     this.toggleChange518 = this.toggleChange518.bind(this);
     this.toggleChange516 = this.toggleChange516.bind(this);
     this.toggleChange515 = this.toggleChange515.bind(this);
     this.toggleChange514 = this.toggleChange514.bind(this);
     this.toggleChange513 = this.toggleChange513.bind(this);
-    //this.fetchDataFromURL = this.fetchDataFromURL.bind(this);
+    this.getBasicNutrients = this.getBasicNutrients.bind(this);
+    this.toggleCheckBoxChange = this.toggleCheckBoxChange.bind(this);    
     this.handleUserInputButton = this.handleUserInputButton.bind(this);
-
+    
   }
 
+  
   toggleChange509 = () => {
     this.setState({
       isCheckBox509Checked: !this.state.isCheckBox509Checked
@@ -111,6 +115,46 @@ class CompareFoodByNutrients extends Component {
     });
   }
 
+  toggleCheckBoxChange(nutriID){
+    const basicNutriInfo = Array.from(this.state.basicNutrientInfo);
+    const nutrient = basicNutriInfo.filter(nutrient =>nutrient.nutrient_id === nutriID);
+    nutrient.isCheckBoxChecked = !'isCheckBoxChecked';
+    //basicNutriInfo[nutriID].isCheckBoxChecked = !this.state.isCheckBoxChecked;
+    this.setState({
+      basicNutrientInfo: basicNutriInfo
+    });
+  }
+
+  getBasicNutrients(){
+    const bNutri = basicNutrients;
+    const newBNutri = [];
+    for(let item in bNutri){
+      // newBNutri[bNutri[item].nutrient_id] = {
+      //   'nutrient_id': bNutri[item].nutrient_id,
+      //   'nutrient_name': bNutri[item].nutrient_name,
+      //   'nutrient_group': bNutri[item].nutrient_group,
+      //   'isCheckBoxChecked': false
+      // }
+      newBNutri[item] = {
+        'nutrient_id': bNutri[item].nutrient_id,
+        'nutrient_name': bNutri[item].nutrient_name,
+        'nutrient_group': bNutri[item].nutrient_group,
+        'isCheckBoxChecked': false
+      }
+    }
+    this.setState({
+      basicNutrientInfo: newBNutri
+    });
+
+  }
+  componentWillMount(){
+    //console.log(newBNutri);
+    //console.log(this.state.basicNutrientInfo);
+  }
+  componentDidMount(){
+    //this.getBasicNutrients();
+  }
+
   componentDidUpdate(prevProps, prevState, props){
     if(this.state.url!==prevState.url && this.state.url!==""){
       fetch(this.state.url, {
@@ -127,6 +171,7 @@ class CompareFoodByNutrients extends Component {
   handleUserInputButton(){
     //check for all true values and create URL as a string and set the state of URL
     //const selectedNutriList = new Set(this.state.selectedNutrients);
+    this.getBasicNutrients();
     const selectedNutriList = new Set();
     let urlTemp = "";
     for(const attri in this.state){
@@ -200,22 +245,34 @@ class CompareFoodByNutrients extends Component {
   }
 
   render() {  
-    return (
+    //console.log(this.state.basicNutrientInfo);
+    const bNutri = Array.from(this.state.basicNutrientInfo);
+    console.log(bNutri);
+    // const temp1000 = bNutri.filter(item => item.nutrient_group === 'Proximates');
+    // console.log(temp1000);
+    
+    //console.log(temp100);
+    return(
       <div>
-      <div>
-        <h1>Nutrients Tree</h1>
         <div>
-            <h2>Amino Acids</h2>
-            <h3 key = '509'><input type ='checkbox' checked = {this.state.isCheckBox509Checked} onChange={this.toggleChange509} />Tyrosine</h3>
-            <h3 key = '518'><input type ='checkbox' checked = {this.state.isCheckBox518Checked} onChange={this.toggleChange518} />Serine</h3>
-            <h3 key = '516'><input type ='checkbox' checked = {this.state.isCheckBox516Checked} onChange={this.toggleChange516} />Glycine</h3>
-            <h3 key = '515'><input type ='checkbox' checked = {this.state.isCheckBox515Checked} onChange={this.toggleChange515} />Glutamic</h3>
-            <h3 key = '514'><input type ='checkbox' checked = {this.state.isCheckBox514Checked} onChange={this.toggleChange514} />Aspartic</h3>
-            <h3 key = '513'><input type ='checkbox' checked = {this.state.isCheckBox513Checked} onChange={this.toggleChange513} />Alanine</h3>
-            <button type='Submit' onClick ={this.handleUserInputButton} >Submit</button>
-        </div>
+          <h1>Nutrients Tree</h1>
+          <div>
+              <h2>Amino Acids</h2>
+              <h3 key = '509'><input type ='checkbox' checked = {this.state.isCheckBox509Checked} onChange={this.toggleChange509} />Tyrosine</h3>
+              <h3 key = '518'><input type ='checkbox' checked = {this.state.isCheckBox518Checked} onChange={this.toggleChange518} />Serine</h3>
+              <h3 key = '516'><input type ='checkbox' checked = {this.state.isCheckBox516Checked} onChange={this.toggleChange516} />Glycine</h3>
+              <h3 key = '515'><input type ='checkbox' checked = {this.state.isCheckBox515Checked} onChange={this.toggleChange515} />Glutamic</h3>
+              <h3 key = '514'><input type ='checkbox' checked = {this.state.isCheckBox514Checked} onChange={this.toggleChange514} />Aspartic</h3>
+              <h3 key = '513'><input type ='checkbox' checked = {this.state.isCheckBox513Checked} onChange={this.toggleChange513} />Alanine</h3>
+              {
+                bNutri.map(item =>{
+                  return(<h3 key ={item.nutrient_id}><input type = 'checkbox' checked = {item.isCheckBoxChecked} onChange = {this.toggleCheckBoxChange(item.nutrient_id)}/>{item.nutrient_name}</h3>)
+                })
+              }
 
-      </div>        
+              <button type='Submit' onClick ={this.handleUserInputButton} >Submit</button>
+          </div>
+        </div>        
         <div className="CompareFoodByNutrients">
           <h2>List all Nutrient ids</h2>
           <ReactTable
